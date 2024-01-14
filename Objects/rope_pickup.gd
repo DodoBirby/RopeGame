02296364e084bea7587915cc2346ff32c72fb5dd
Xander <100409014+TheyCallMeXander@users.noreply.github.com>
2024-01-13 20:00:53 -0600

    Fix Pickup

--- Objects/rope_pickup.gd
+++ Objects/rope_pickup.gd
@@ -1,7 +1,10 @@
-extends Sprite2D
+extends AnimatedSprite2D
 
 
 func _on_pickup_radius_body_entered(body):
 	if body is Player:
 		body.rope_pickup()
 		queue_free()
+
+func _ready():
+	play("default")
