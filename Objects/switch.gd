a8ce867268e8bb66767af65e70738e3739bebfd5
Xander <100409014+TheyCallMeXander@users.noreply.github.com>
2024-01-23 18:36:33 -0600

    Synced Switch states on same layer

--- Objects/switch.gd
+++ Objects/switch.gd
@@ -6,6 +6,8 @@ extends Area2D
 @export var enabled: bool = false
 @onready var sprite = $Sprite2D
 
+var prevenabled = enabled
+
 func _ready():
 	tilemap = get_parent()
 	tilemap.set_layer_enabled(layer_to_switch, enabled)
@@ -13,12 +15,15 @@ func _ready():
 func _on_body_entered(body):
 	if body is Player:
 		body.switchnearby = self
-	
+
 func _physics_process(delta):
 	if enabled:
 		sprite.region_rect.position = Vector2.ZERO
 	else:
 		sprite.region_rect.position = Vector2(16, 0)
+	if tilemap.is_layer_enabled(layer_to_switch) != prevenabled:
+		enabled = tilemap.is_layer_enabled(layer_to_switch)
+	prevenabled = enabled
 
 
 func _on_body_exited(body):
